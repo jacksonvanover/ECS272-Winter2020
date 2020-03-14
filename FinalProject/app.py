426aa601c7867c0aeacacfa5d6b7006e88b61aef
jacksonvanover <jacksonvanover@gmail.com>
2020-03-14 15:57:34 -0700

    added some callbacks

--- FinalProject/app.py
+++ FinalProject/app.py
@@ -3,12 +3,17 @@ import numpy as np
 import subprocess
 import os
 import pickle
+from sklearn.cluster import KMeans
 
 import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
+
+import plotly.express as px
+
+from Embedding_concat import concat_embeddings
 
 '''
 COLUMN_NAMES :
@@ -22,6 +27,7 @@ COLUMN_NAMES :
         'represented_features_embeddings',
         'word2vec_embeddings'
 '''
+
 def collapsible_item(i):
     # we use this function to make the collapsible items to avoid code duplication
     return dbc.Card(
@@ -64,7 +70,40 @@ def render_visualization():
                     html.Div(
                         children=[
                             html.Div(
-                                children=dcc.Markdown('# sliders go here'),
+                                children=[
+                                    dcc.Slider(
+                                        id='cluster_slider', 
+                                        min= 2, 
+                                        max= 5, 
+                                        value= 2, 
+                                        marks={str(num): str(num) for num in range(2,6)}, 
+                                        step=None
+                                    ),
+                                    dcc.Slider(
+                                        id='wt1_slider', 
+                                        min= 0, 
+                                        max= 1, 
+                                        value= 1, 
+                                        marks={str(num): str(num) for num in range(0,2)}, 
+                                        step=0.01
+                                    ),
+                                    dcc.Slider(
+                                        id='wt2_slider', 
+                                        min= 0, 
+                                        max= 1, 
+                                        value= 1, 
+                                        marks={str(num): str(num) for num in range(0,2)}, 
+                                        step=0.01
+                                    ),
+                                    dcc.Slider(
+                                        id='wt3_slider', 
+                                        min= 0, 
+                                        max= 1, 
+                                        value= 1, 
+                                        marks={str(num): str(num) for num in range(0,2)}, 
+                                        step=0.01
+                                    ),
+                                ],
                                 style={
                                     'height' : '25em',
                                     'backgroundColor' : 'rgba(0,0,0,0.01)'
@@ -90,7 +129,7 @@ def render_visualization():
                 # column with scatterplot
                 dbc.Col(
                     html.Div(
-                        children=dcc.Markdown("# Scatterplot goes here"),
+                        children=dcc.Graph(figure=px.scatter(x=[0,1,2,3,4], y=[0,1,4,9,16]),id='scatter_plot'),
                         style={
                             'backgroundColor' : 'rgb(0,220,0)',
                             'height' : '50em'
@@ -104,8 +143,35 @@ def render_visualization():
                     html.Div(
                         children=html.Div(
                                     children=[
-                                        collapsible_item("Data Coverage Board"),
-                                        collapsible_item("Cluster Board")
+                                        dbc.Card(
+                                            children=[
+                                                        dbc.CardHeader(
+                                                            dbc.Button(
+                                                                "Data Coverage Board",
+                                                                color="link",
+                                                                id="data_coverage_board_toggle",
+                                                            )
+                                                        ),
+                                                        dbc.Collapse(
+                                                            dbc.CardBody("Data Coverage Board goes here..."),
+                                                            id="collapse_data_coverage_board",
+                                                        ),
+                                                    ]
+                                            ),
+                                        dbc.Card(
+                                            children=[
+                                                        dbc.CardHeader(
+                                                            dbc.Button(
+                                                                "Cluster Board",
+                                                                color="link",
+                                                                id="cluster_board_toggle",
+                                                            )
+                                                        ),
+                                                        dbc.Collapse(
+                                                            id="collapse_cluster_board",
+                                                        ),
+                                                    ]
+                                            )
                                     ],
                                     className="accordion"
                                 ),
@@ -120,13 +186,15 @@ def render_visualization():
         ]
     )
 
+    # callback to toggle the collapsible data coverage board and
+    # cluster board
     @app.callback(
-        [Output(f"collapse_Data Coverage Board", "is_open"),
-        Output(f"collapse_Cluster Board", "is_open")],
-        [Input(f"Data Coverage Board_toggle", "n_clicks"),
-        Input(f"Cluster Board_toggle", "n_clicks")],
-        [State(f"collapse_Data Coverage Board", "is_open"),
-        State(f"collapse_Cluster Board", "is_open")],
+        [Output("collapse_data_coverage_board", "is_open"),
+        Output("collapse_cluster_board", "is_open")],
+        [Input("data_coverage_board_toggle", "n_clicks"),
+        Input("cluster_board_toggle", "n_clicks")],
+        [State("collapse_data_coverage_board", "is_open"),
+        State("collapse_cluster_board", "is_open")],
     )
     def toggle_accordion(n1, n2, is_open1, is_open2):
         ctx = dash.callback_context
@@ -136,11 +204,48 @@ def render_visualization():
         else:
             button_id = ctx.triggered[0]["prop_id"].split(".")[0]
 
-        if button_id == "Data Coverage Board_toggle" and n1:
+        if button_id == "data_coverage_board_toggle" and n1:
             return not is_open1, is_open2
-        elif button_id == "Cluster Board_toggle" and n2:
+        elif button_id == "cluster_board_toggle" and n2:
             return is_open1, not is_open2
         return True, True
+
+
+    # callback to display stats on the cluster selected from the scatterplot
+    @app.callback(
+        Output("collapse_cluster_board", "children"),
+        [Input("scatter_plot", "clickData")]
+    )
+    def generate_cluster_board(click_data):
+        if not click_data:
+            return dbc.CardBody("Default Cluster Board stats"),
+        else:
+            return dbc.CardBody("Updated Cluster board stats")
+
+
+    # callback to update dashboard with slider values
+    @app.callback(
+            [Output('scatter_plot', 'figure')],
+            [Input('wt1_slider', 'value'),
+            Input('wt2_slider', 'value'),
+            Input('wt3_slider', 'value'),
+            Input('cluster_slider', 'value')]
+    )
+    def update_chart(wt1, wt2, wt3, k):
+        concat_embeddings(df, wt1, wt2, wt3)
+        embedding_combo_array = np.array(df['concat_embedding'].tolist())
+        km = KMeans(n_clusters=k, random_state=10).fit(embedding_combo_array)
+        df['cluster'] = km.labels_ 
+        
+        #insert call to recalculate PCA#
+        
+        # fig = px.scatter(df, x="PC1", y="PC2", color="cluster",
+        #         hover_data=['title'])
+
+        # temporary scatterplot
+        fig = px.scatter(x=[0,1,2,3,4], y=[0,1,4,9,16])
+
+        return [fig]
 
     app.run_server(debug=True)
 
